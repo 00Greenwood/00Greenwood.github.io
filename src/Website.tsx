e459a8e46b5dea714a00fdfea6e8192b01a07fa7
00Greenwood <marcus_greenwood@hotmail.co.uk>
2022-07-24 20:07:01 -0400

    Add Name

--- src/Website.tsx
+++ src/Website.tsx
@@ -1,14 +1,14 @@
-import { Avatar, Box } from "@mui/material";
-import { useState } from "react";
+import { Avatar, Container, Typography } from "@mui/material";
 
 export const Website = () => {
   return (
-    <>
+    <Container sx={{backgroundColor: "primary.dark"}}>
       <Avatar
         alt="Marcus Greenwood"
         src="src\assets\profile.jpg"
         sx={{ width: 128, height: 128 }}
       />
-    </>
+      <Typography variant="h1" component="div" gutterBottom color="primary.light">Marcus Greenwood</Typography>
+      </Container>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Only need to import the font once at the entry point.
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Website } from "./Website";
